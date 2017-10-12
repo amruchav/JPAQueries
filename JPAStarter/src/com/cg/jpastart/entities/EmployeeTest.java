64b9e8a883f045cde15535cf79aeabdf7b253c9b
amruchav <32138342+amruchav@users.noreply.github.com>
2017-10-12 17:43:14 +0530

    Add files via upload

---
+++ JPAStarter/src/com/cg/jpastart/entities/EmployeeTest.java
@@ -0,0 +1,33 @@
+package com.cg.jpastart.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EmployeeTest {
+
+		public static void main(String[] args) {  
+			EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+			EntityManager em = factory.createEntityManager();
+			em.getTransaction().begin();
+			
+		   Employee e1=new Employee();  
+		    e1.setId(1005);  
+		    e1.setFirstName("Ajita");  
+		    e1.setLastName("Gaikwad"); 
+		    
+		  
+		    Employee e2=new Employee();  
+		    e2.setId(1006);  
+		    e2.setFirstName("Sudhir");  
+		    e2.setLastName("Gaikwad");  
+		      
+		   em.persist(e1);  
+		    em.persist(e2);  
+			em.getTransaction().commit();
+		    System.out.println("successfully saved"); 
+		    
+		    em.close();
+			factory.close();
+		}  
+		} 
